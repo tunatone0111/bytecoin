6ff9c1d58996ec73305e3fbc723414da76335111
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-10-14 22:18:29 +0900

    [feature] add mongodb configuration file example

---
+++ crawlers/mongodb_config_example.py
@@ -0,0 +1,9 @@
+mongoconfig = {
+    'username': 'myusername',
+    'password': 'secret',
+    'host': 'example.com',
+    'port': 27017,
+    'database': 'bytecoin',
+}
+
+# configure your mongodb and save it to mongodb_config.py
