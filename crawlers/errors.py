2a2cba381c6d08c7b712b3973a069d7d90499b13
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-10-14 22:01:33 +0900

    [feature] add HTMLElementNotFoundException

--- crawlers/errors.py
+++ crawlers/errors.py
@@ -2,5 +2,12 @@ class StockNotFoundException(Exception):
     def __init__(self, message):
         super(StockNotFoundException, self).__init__(message)
 
+
 class DateNotInRangeException(Exception):
-    pass
+    def __init__(self, message):
+        super(DateNotInRangeException, self).__init__(message)
+
+
+class HTMLElementNotFoundException(Exception):
+    def __init__(self, message):
+        super(HTMLElementNotFoundException, self).__init__(message)
