2209dea87d606dec38631d4304568a164fa8a0c2
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-10-14 22:21:14 +0900

    [refractor] refractored stock_info.py
    - removed unnecessary multiprocessing feature
    - seperated stock sources and errors
    - simplified methods and removed flags(option)
    - added comments

--- crawlers/stock_info.py
+++ crawlers/stock_info.py
@@ -1,23 +1,29 @@
+# system packages
 from bs4 import BeautifulSoup
 import numpy as np
 import requests
 import pandas as pd
 from fake_useragent import UserAgent
 import json
-from abc import *
-from pprint import pprint
-from stock_code import read_stock_codes_from_db, get_stock_code
-from urltools import qdic
+
+# crawler packages
+from urltools import get_query
 from mongodb import get_db
-from multiprocessing import Process
-from errors import DateNotInRangeException
+from stock_sources import NAVER
+from errors import DateNotInRangeException, HTMLElementNotFoundException
+
+# constants
+TWO_DIGIT_TEMPLATE = "{0:0=2d}"
 
 
 class Crawler():
     def __init__(self):
+        # initialize fake user agent
         ua = UserAgent(verify_ssl=False)
         userAgent = ua.random
         self.headers = {'User-Agent': userAgent}
+
+        # initialize result array
         self.result = []
 
 
@@ -25,79 +31,86 @@ class NaverCrawler(Crawler):
     def template(self, stock_code, page):
         return f"https://finance.naver.com/item/board.nhn?code={stock_code}&page={page}"
 
-    def crawl_from_page(self, stock_code, max_pages, syncDB=False):
-        self.result = []
-        for page in range(1, 1 + max_pages):
-            print(f"[page] ({page}/{max_pages})")
-            self.crawl_page(stock_code, page, syncDB)
+    def crawl(self, stock_code, max_pages, date):
+        self.result = []  # flush result array
 
-    def crawl_from_date(self, stock_code, max_pages, date, syncDB=False):
-        self.result = []
         for page in range(1, 1 + max_pages):
             print(f"[page] ({page}/{max_pages})")
-            done = self.crawl_page(stock_code, page, syncDB, True, date)
+            done = self.crawl_page(stock_code, page, date)
             if done:
                 break
 
-    def crawl_page(self, stock_code, page, syncDB=False, date_filter=False, date={}):
+    def crawl_page(self, stock_code, page, date):
         url = self.template(stock_code, page)
         html = requests.get(url, headers=self.headers).text
         soup = BeautifulSoup(html, 'html5lib')
 
         # find <a> tag elements whose link navigates to each post
-        a_tag_elements = soup.select(
-            '#content td.title > a')
+        a_tag_elements = soup.select('#content td.title > a')
+
+        # when we request page 100 on a stock that has only 50 pages, it returns the current page as '50'.
+        # therefore, mismatch between requested page number, and the responded page means that
+        # the crawler has reached the end page.
         if page != int(soup.select_one('td.on').get_text()):
             return True
 
+        # get all post links.
         post_links = []
         for a_tag_element in a_tag_elements:
             post_links.append("https://finance.naver.com" +
                               a_tag_element['href'])
+
+        # visit all post links and crawl them.
         for post_link in post_links:
             try:
-                post = self.crawl_post(
-                    post_link, date, date_filter)
+                post = self.crawl_post(post_link, date)
             except DateNotInRangeException as e:
                 print(e)
-                break
+                break  # stop crawling when post date is earlier than the limit
             self.result.append(post)
-            if syncDB:
-                Posts = get_db()['Posts']
-                if not Posts.find_one({'id': post['id']}):
-                    Posts.insert_one(post)
 
-    def crawl_post(self, post_link, date={}, date_filter=False):
+    def crawl_post(self, post_link, date):
         try:
             html = requests.get(post_link, headers=self.headers).text
             soup = BeautifulSoup(html, 'html5lib')
 
-            qmap = qdic(post_link)
-            post_id = qmap['nid'][0]
-            stock_code = qmap['code'][0]
-            post_page = int(qmap['page'][0])
+            query_dict = get_query(post_link)
+
+            # get metadata from querystring
+            post_id = query_dict['nid'][0]
+            stock_code = query_dict['code'][0]
             post_title = "undefined"
-            titleel = soup.select_one("strong.c.p15")
-            if titleel:
-                post_title = titleel.get_text()
-            print(f'[post] {post_title}')
+
+            # todo - specifiy title element in a more reliable way.
+            title_element = soup.select_one("strong.c.p15")
+            if title_element:
+                post_title = title_element.get_text()
+
             post_content = soup.select_one("#body").get_text('\n', strip=True)
             post_date = soup.select_one(".gray03.p9.tah").get_text()
-            if date_filter:
-                year = date['year']
-                month = date['month']
-                day = date['day']
-                compareable = f'{year}.{"{0:0=2d}".format(month)}.{"{0:0=2d}".format(day)}'
-                if post_date < compareable:
-                    print(f'end. {post_date}',
-                          compareable)
-                    raise DateNotInRangeException()
+
+            # filter dates
+            year = date['year']
+            month = TWO_DIGIT_TEMPLATE.format(date['month'])  # 1 => 01
+            day = TWO_DIGIT_TEMPLATE.format(date['day'])  # 9 => 09
+            compareable = f'{year}.{month}.{day}'
+
+            # "2020.09.11" > "2020.09.10 12:30:15"
+            # if the post date is earlier than the given date(comparable)
+            if post_date < compareable:
+                print(f'end. {post_date}',
+                      compareable)
+                # stop crawling for this post
+                raise DateNotInRangeException("date is not in range")
+
             post_views = soup.select_one("span.tah.p11").get_text()
             post_good_count = soup.select_one("._goodCnt").get_text()
             post_bad_count = soup.select_one("._badCnt").get_text()
 
+            # construct post object
             post = {
                 'id': post_id,
+                'source': NAVER,
                 'code': stock_code,  # stock code
                 'link': post_link,
                 'title': post_title,
@@ -107,12 +120,16 @@ class NaverCrawler(Crawler):
                 'good_count': post_good_count,
                 'bad_count': post_bad_count,
             }
+
             return post
+
         except AttributeError as e:
+            # catches if any HTML element does not exist.
             print(e)
-            raise Exception("Parsing Failed")
+            raise HTMLElementNotFoundException("Parsing Failed")
 
 
+# example code
 nc = NaverCrawler()
 
 fr = {
@@ -121,14 +138,6 @@ fr = {
     'day': 8
 }
 
-p1 = Process(target=nc.crawl_from_date, args=('208850', 10, fr, True))
-p2 = Process(target=nc.crawl_from_date, args=('223220', 10, fr, True))
-p3 = Process(target=nc.crawl_from_date, args=('068270', 10, fr, True))
-
-p1.start()
-p2.start()
-p3.start()
-
-p1.join()
-p2.join()
-p3.join()
+print('start crawling...')
+nc.crawl('208850', 2, fr)
+print(nc.result)
--- crawlers/urltools.py
+++ crawlers/urltools.py
@@ -1,8 +1,15 @@
 from urllib.parse import urlparse, parse_qs
 
-def qdic(url):
-  parts = urlparse(url)
-  return parse_qs(parts.query)
 
-def replace_query_string(url, key, value):
-  qs = dict(parse_qsl(parts.query))
+def get_query(url):
+    """Parses the query part of an url to dictionaries.
+    ex) 
+
+    Args:
+        url (string): url
+
+    Returns:
+        dict: query dictionary
+    """
+    parts = urlparse(url)
+    return parse_qs(parts.query)
