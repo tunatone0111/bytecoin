849ca37b7e9a0adafd0c3f83b9ea8bd4ad86e8e9
tunatone0111 <tunatone.0111@gmail.com>
2020-11-03 20:54:58 +0900

    [refactor] server refactoring

---
+++ server/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+
+from app.config import config_by_name
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
---
+++ server/app/config.py
@@ -0,0 +1,31 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.getenv('BYTECOIN_SECRET_KEY', 'bytecoinsecretkey')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
