9105e94caf32d2a45c62f032b3167abc4e9d32df
tunatone0111 <tunatone0111@gmail.com>
2020-11-10 19:11:37 +0000

    [feat] add config, db module

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,16 +2,22 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 
-from app.config import config_by_name
+from .config import config_by_name
+from .db import get_db
 
 
 def create_app(config_name):
-    app = Flask(__name__, static_url_path='',
-                static_folder='../views', template_folder='../views')
+    app = Flask(__name__,
+                static_url_path='',
+                static_folder='../views',
+                template_folder='../views')
     app.config.from_object(config_by_name[config_name])
+
+    db = get_db()
 
     @app.route('/')
     def index():
+        print(db['Stocks'])
         return render_template('index.html')
 
     return app
