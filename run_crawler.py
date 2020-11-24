4a3a0b6ad83c6ac8d7f0782894dc89f0d633d9d1
tunatone0111 <tunatone0111@gmail.com>
2020-11-24 14:38:50 +0000

    [fix] fix run_crawler

--- run_crawler.py
+++ run_crawler.py
@@ -11,8 +11,6 @@ from datetime import date, timedelta
 import multiprocessing
 from server.app.config import kospi_100
 
-NSAMPLE = 20
-
 db = get_db()
 stocks = db['Stocks']
 
@@ -30,10 +28,7 @@ print('crawling target day: ', fr)
 MAX_PAGE = 1
 pc = Price_Crawler()
 
-crawl_list = list(target_stocks)
-print(crawl_list)
-
-def crawl_price():
+def crawl_price(crawl_list):
     print('start crawling...')
     for stock in crawl_list:
         result = pc.crawl_page(stock=stock, multi_processed=True)
@@ -41,19 +36,19 @@ def crawl_price():
         if 'price' not in stock or stock['price'] is None:
             stock['price'] = [cur_price]
         else:
+            if stock['price'][0]['price'] == cur_price['price']:
+                continue
             stock['price'] = stock['price'][:99]
             stock['price'].insert(0, cur_price)
         stocks.update_one({'_id': stock['_id']}, {
                         "$set": {'price': stock['price']}})
-        print(result)
 
-def crawl_posts():
+def crawl_posts(crawl_list):
     nc = NaverCrawler()
 
     print('start crawling...')
     for stock in crawl_list:
         res = nc.crawl(stock['code'], MAX_PAGE, fr)
-        print(res)
 
 if __name__ == '__main__':
-    pass
+    crawl_posts(target_stocks)
