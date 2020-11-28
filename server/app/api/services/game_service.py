0867a82c6515e7787fd895a614935c6322d9baef
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-11-28 13:58:07 +0000

    [refactor] extract validation logic

--- server/app/api/services/game_service.py
+++ server/app/api/services/game_service.py
@@ -11,51 +11,51 @@ def get_stocks_info(user_id="5fbd6ce41691156838210650"):
     return {"stocks": stocks}
 
 
-def buy_stock(stock_code, qty, user_id="5fbd6ce41691156838210650"):
-    if qty < 0:
-        raise InvalidTradeException('invalid qty')
+def _validation_arguments(f):
+    def wrapper(stock_code, qty, user_id="5fbd6ce41691156838210650"):
+        if qty < 0:
+            raise InvalidTradeException('invalid qty')
 
-    user = read_user(user_id)
+        user = read_user(user_id)
 
-    if stock_code not in user['stocks'].keys():
-        raise InvalidTradeException('invalid stock id')
+        if stock_code not in user['stocks'].keys():
+            raise InvalidTradeException('invalid stock id')
 
+        return f(stock_code, qty, user_id, user)
+    return wrapper
+
+
+@_validation_arguments
+def buy_stock(stock_code, qty, user_id, _user=None):
     cur_price = TEST_PRICE
 
-    new_balance = user['balance'] - cur_price * qty
-    if new_balance < 0:
+    new_cash = _user['cash'] - cur_price * qty
+    if new_cash < 0:
         raise InvalidTradeException('not enough money')
 
-    new_qty = qty + user['stocks'][stock_code]
+    new_qty = qty + _user['stocks'][stock_code]
 
-    update_user(user_id, {'balance': new_balance,
+    update_user(user_id, {'cash': new_cash,
                           f'stocks.{stock_code}': new_qty})
 
-    return {"updated_balance": new_balance,
+    return {"updated_cash": new_cash,
             "updated_stock_id": stock_code,
             "updated_qty": new_qty}
 
 
-def sell_stock(stock_code, qty, user_id="5fbd6ce41691156838210650"):
-    if qty < 0:
-        raise InvalidTradeException('invalid qty')
-
-    user = read_user(user_id)
-
-    if stock_code not in user['stocks']:
-        raise InvalidTradeException('invalid stock id')
-
-    new_qty = user['stocks'][stock_code] - qty
+@_validation_arguments
+def sell_stock(stock_code, qty, user_id, _user=None):
+    new_qty = _user['stocks'][stock_code] - qty
     if new_qty < 0:
         raise InvalidTradeException('not enough qty')
 
     cur_price = TEST_PRICE
 
-    new_balance = user['balance'] + cur_price * qty
+    new_cash = _user['cash'] + cur_price * qty
 
-    update_user(user_id, {'balance': new_balance,
+    update_user(user_id, {'cash': new_cash,
                           f'stocks.{stock_code}': new_qty})
 
-    return {"updated_balance": new_balance,
+    return {"updated_cash": new_cash,
             "updated_stock_id": stock_code,
             "updated_qty": new_qty}
