8cf8421f24d114eb6b71f551c50c9285533cea79
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-11-28 10:35:28 +0000

    [feat/refactor] add stocks controller, service

---
+++ server/app/api/stocks.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify, make_response
+
+from ..config import kospi_100
+from ..stock_service import read_all, read_top5, read_one
+
+stocks = Blueprint('stocks', __name__)
+
+
+@stocks.route('/')
+def get_all():
+    result = read_all()
+    assert(any(result))
+    return jsonify(stocks=result)
+
+
+@stocks.route('/top')
+def get_top():
+    result = read_top5()
+    assert(any(result))
+    return jsonify(stocks=result)
+
+
+@stocks.route('/<code>')
+def get_one(code):
+    result = read_one(code)
+    assert(any(result))
+    return jsonify(stocks=result)
+
+
+@stocks.errorhandler(AssertionError)
+def handle_None(e):
+    print(e)
+    return jsonify(stocks=None, errmsg='Not Found'), 404
--- server/app/stock_service.py
+++ server/app/stock_service.py
@@ -5,22 +5,33 @@ demo_flag = True
 stocks_coll_name = 'StocksSample' if demo_flag else 'Stocks'
 
 db = get_db()
+stocks = db[stocks_coll_name]
+posts = db['Posts']
+
+dto_projection = {'name': True, 'code': True,
+                  '_id': False, 'label': True}
+
+
+def read_all():
+    return list(stocks.find({}, dto_projection))
+
+
+def read_top5():
+    return list(stocks.aggregate([{'$project': dto_projection}, {'$sort': {'label': -1}}, {'$limit': 5}]))
+
+
+def read_one(code):
+    return list(stocks.find({'code': code}, dto_projection))
 
 
 def get_stock_list():
     return kospi_100
 
 
-def get_top5_stocks():
-    return list(db[stocks_coll_name].aggregate([{'$sort': {'label': -1}}, {'$limit': 5}]))
-
-
 def get_current_stock_price(code):
     """ returns dict {'date': , 'price': } """
-    stocks = db[stocks_coll_name]
     return stocks.find_one({'code': code}, {'price': True})['price'][0]
 
 
 def get_posts_by_stock_code(code):
-    posts = db['Posts']
     return list(posts.find({'code': code}))
